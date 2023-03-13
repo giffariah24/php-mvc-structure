72a7a5312198c29f91b38b8eaac9d04ec4779430
giffariah666 <giffariah@gmail.com>
2023-03-13 18:01:47 +0700

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("BASEURL", "http://localhost/php-todos/public");
+
+// DB
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "phptodos");
+define("DB_PORT", "3305");
---
+++ app/views/home/index.php
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1 class="display-4 text-center">PHP TODOS APP</h1><br>
+    <p class="text-center">If you don't have an account, create here <a href="/register">register</a></p>
+</div>
